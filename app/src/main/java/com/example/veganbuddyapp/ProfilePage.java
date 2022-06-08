2be07d930c2f6ec0a88fb6415f86bb40a8ae975e
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-06-08 08:42:32 -0400

    fixing

--- app/src/main/java/com/example/veganbuddyapp/ProfilePage.java
+++ app/src/main/java/com/example/veganbuddyapp/ProfilePage.java
@@ -22,24 +22,24 @@ public class ProfilePage extends AppCompatActivity {
     public NavigationView navigationView;
 
 
-        navigationView = findViewById(R.id.navView);
+    navigationView = findViewById(R.id.navView);
 
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+    // drawer layout instance to toggle the menu icon to open
+    // drawer and back button to close drawer
+    drawerLayout = findViewById(R.id.my_drawer_layout);
+    actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+    // pass the Open and Close toggle for the drawer layout listener
+    // to toggle the button
+    drawerLayout.addDrawerListener(actionBarDrawerToggle);
+    actionBarDrawerToggle.syncState();
 
-        // to make the Navigation drawer icon always appear on the action bar
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
+    // to make the Navigation drawer icon always appear on the action bar
+    Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
                     case R.id.nav_account:
                         Intent intent = new Intent(ProfilePage.this,ProfilePage.class);
                         startActivity(intent);
