1407f3131949c922af5c66b2d2981f8c7fcd782e
manjitsidhu77 <101609020+manjitsidhu77@users.noreply.github.com>
2022-06-08 08:06:30 -0400

    ok

--- app/src/main/java/com/example/veganbuddyapp/ProfilePage.java
+++ app/src/main/java/com/example/veganbuddyapp/ProfilePage.java
@@ -8,17 +8,6 @@ import android.widget.Button;
 public class ProfilePage extends AppCompatActivity {
     Button backButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
 
-        backButton = findViewById(R.id.back_Button);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
+
 }
