939905004bec439f3b22977bc59d4fca30321f91
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-06-14 11:39:27 -0400

    learning

--- app/src/main/java/com/example/veganbuddyapp/PaymentPage.java
+++ app/src/main/java/com/example/veganbuddyapp/PaymentPage.java
@@ -10,11 +10,13 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
 public class PaymentPage extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
     public NavigationView navigationView;
@@ -23,6 +25,7 @@ public class PaymentPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_page);
+        mAuth = FirebaseAuth.getInstance();
 
         navigationView = findViewById(R.id.navView);
 
