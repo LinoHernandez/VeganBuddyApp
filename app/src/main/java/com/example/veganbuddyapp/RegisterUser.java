1783b5c8bcaf7198021f68c46c48a67d41aed9e2
TejinderSing <49978995+TejinderSing@users.noreply.github.com>
2022-06-14 10:35:01 -0400

    Register user

--- app/src/main/java/com/example/veganbuddyapp/RegisterUser.java
+++ app/src/main/java/com/example/veganbuddyapp/RegisterUser.java
@@ -3,12 +3,21 @@ package com.example.veganbuddyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterUser extends AppCompatActivity {
-
+    EditText registerName, registerPassword, registerPhone, registerEmail;
+    Button registerUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        registerEmail = findViewById(R.id.registerEmail);
+        registerName = findViewById(R.id.registerName);
+        registerPassword = findViewById(R.id.registerPassword);
+        registerPhone = findViewById(R.id.registerPhone);
+        registerUser = findViewById(R.id.registerUser);
     }
 }
