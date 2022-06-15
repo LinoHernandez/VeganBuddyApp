89f6cc43ffc09c5695200c626ac84d05b1d1a5d1
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-06-15 08:36:55 -0400

    refactor name

--- app/src/main/java/com/example/veganbuddyapp/RegisterPage.java
+++ app/src/main/java/com/example/veganbuddyapp/RegisterPage.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -20,10 +19,8 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
@@ -41,7 +38,7 @@ public class RegisterPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logregister_screen);
+        setContentView(R.layout.register_screen);
         mAuth = FirebaseAuth.getInstance();
 
         navigationView = findViewById(R.id.navView);
