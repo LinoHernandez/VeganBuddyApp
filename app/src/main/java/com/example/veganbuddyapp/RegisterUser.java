1d689ff2e75059c872d419eac5bde62c306f5d90
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-06-16 09:14:34 -0400

    bruh

--- app/src/main/java/com/example/veganbuddyapp/RegisterUser.java
+++ app/src/main/java/com/example/veganbuddyapp/RegisterUser.java
@@ -1,39 +1,36 @@
 package com.example.veganbuddyapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
-import android.view.View;
+import android.text.TextUtils;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-<<<<<<< HEAD
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-=======
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
->>>>>>> parent of 1acae23 (database)
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 public class RegisterUser extends AppCompatActivity {
     EditText registerName, registerPassword, registerPhone, registerEmail;
-    Button registerUser;
-
-    String str_name,str_pass,str_mail,str_phone;
-
-    private ProgressDialog progressDialog;
+    Button login,registerButton;
+    private FirebaseAuth mAuth;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,33 +41,65 @@ public class RegisterUser extends AppCompatActivity {
         registerName = findViewById(R.id.registerName);
         registerPassword = findViewById(R.id.registerPassword);
         registerPhone = findViewById(R.id.registerPhone);
-        registerUser = findViewById(R.id.registerUser);
+        registerButton = findViewById(R.id.registerButton);
+        login = findViewById(R.id.login);
 
-        // Progress
+        mAuth = FirebaseAuth.getInstance();
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Please wait...");
-        progressDialog.setCanceledOnTouchOutside(false);
+        navigationView = findViewById(R.id.navView);
 
-        registerUser.setOnClickListener(new View.OnClickListener() {
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+        // to make the Navigation drawer icon always appear on the action bar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Validation();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_account:
+                        Intent intent = new Intent(RegisterUser.this,ProfilePage.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.nav_logout:
+                        Intent register = new Intent(RegisterUser.this, LoginPage.class);
+                        startActivity(register);
+                        return true;
+                    case R.id.nav_payment:
+                        Intent payment = new Intent(RegisterUser.this,PaymentPage.class);
+                        startActivity(payment);
+                        return true;
+                    case R.id.nav_menu:
+                        Intent menu = new Intent(RegisterUser.this,MainActivity.class);
+                        startActivity(menu);
+                        return true;
+                    default:
+                        return true;
+                }
             }
         });
-    }
-    private void Validation() {
-    str_name = registerName.getText().toString();
-    str_mail = registerEmail.getText().toString();
-    str_pass = registerPassword.getText().toString();
-    str_phone = registerPhone.getText().toString();
 
-<<<<<<< HEAD
-    if (str_name.isEmpty()) {
-        registerName.setError("Please enter field");
-        registerName.requestFocus();
-        return;
-=======
+
+
+        registerButton.setOnClickListener(view ->{
+            createUser();
+        });
+
+        login.setOnClickListener(view ->{
+            startActivity(new Intent(RegisterUser.this, LoginPage.class));
+        });
+
+
+    }
+
     private void createUser(){
         String Email = registerEmail.getText().toString();
         String Password = registerPassword.getText().toString();
@@ -94,6 +123,10 @@ public class RegisterUser extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
+                        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                        DatabaseReference databaseReference = firebaseDatabase.getReference().child("Users").child(mAuth.getUid());
+                        User myuserProfile=new User(mAuth.getUid(),String.valueOf(registerName.getText()),String.valueOf(registerEmail.getText()),String.valueOf(registerPhone.getText()));
+                        databaseReference.setValue(myuserProfile);
                         Toast.makeText(RegisterUser.this,"User registered successfully", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(RegisterUser.this, MainActivity.class));
                     }else{
@@ -102,78 +135,31 @@ public class RegisterUser extends AppCompatActivity {
                 }
             });
         }
->>>>>>> parent of 1acae23 (database)
-    }
-    if (!Patterns.EMAIL_ADDRESS.matcher(str_mail).matches()){
-       registerEmail.setError("Please enter valid email");
-       registerEmail.requestFocus();
-       return;
-    }
-    if (str_pass.isEmpty()) {
-        registerPassword.setError("Please enter field");
-        registerPassword.requestFocus();
-        return;
-    }
-    else if (!passwordValidation(str_pass)){
-        registerPassword.setError("enter maximum 6 digits");
-        registerPassword.requestFocus();
-}
-    if (!numberCheck(str_phone)) {
-        registerPhone.setError("Invalid Mobile No.");
-        registerPhone.requestFocus();
-        return;
     }
 
-    createAccount();
-}
+    //@Override
+    //public void onStart() {
+    //super.onStart();
+    // Check if user is signed in (non-null) and update UI accordingly.
+    //FirebaseUser user = mAuth.getCurrentUser();
+    //if (user == null){
+    //startActivity(new Intent(MainActivity.this, RegisterPage.class));
+    //}
+    //updateUI(currentUser);
+    //}
 
-    private boolean passwordValidation(String str_pass) {
-            Pattern p = Pattern.compile(".{6}");
-            Matcher m = p.matcher(str_pass);
-            return m.matches();
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-
-
-    private boolean numberCheck(String str_phone) {
-
-        Pattern p = Pattern.compile("[0,9][11]");
-        Matcher m = p.matcher(str_phone);
-        return m.matches();
+        return super.onOptionsItemSelected(item);
     }
 
-    private void createAccount() {
-        progressDialog.setMessage("Creating Account ");
-        progressDialog.show();
 
-        sendDataToDb();
-    }
-
-    private void sendDataToDb() {
-        String regtime = "" + System.currentTimeMillis();
-        HashMap<String, String> data = new HashMap<>();
-        data.put("name", str_name);
-        data.put("mail", str_mail);
-        data.put("password", str_pass);
-        data.put("phone", str_phone);
-
-        //database
-
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
-        reference.child(str_name).setValue(data)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        progressDialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Registration Successfull", Toast.LENGTH_SHORT).show();
-                    }
-
-                });
-    }}
-
-
-
-
-
+}
 
 
 
