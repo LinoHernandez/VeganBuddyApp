4ad584ec6375114e75e18f974f63196755e585e6
manjitsidhu77 <101609020+manjitsidhu77@users.noreply.github.com>
2022-06-16 08:34:30 -0400

    restaurant page design

---
+++ app/src/main/java/com/example/veganbuddyapp/RestaurantPage.java
@@ -0,0 +1,14 @@
+package com.example.veganbuddyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_page);
+    }
+}
