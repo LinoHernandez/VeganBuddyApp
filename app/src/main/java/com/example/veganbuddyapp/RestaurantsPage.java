9eefce241e9cfd9ccc3c528589163a5caf458e58
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-06-28 08:46:44 -0400

    grid view

--- app/src/main/java/com/example/veganbuddyapp/RestaurantsPage.java
+++ app/src/main/java/com/example/veganbuddyapp/RestaurantsPage.java
@@ -3,12 +3,47 @@ package com.example.veganbuddyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RestaurantsPage extends AppCompatActivity {
+
+    private GridView resList;
+    private static final String API_KEY = "AIzaSyAvhvD5YBxPNO2L4bsN745AF8Bi8fpze7w";
+
+    private static final String PLACES_API_BASE =
+            "https://maps.googleapis.com/maps/api/place";
+
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String TYPE_DETAILS = "/details";
+    private static final String TYPE_SEARCH = "/nearbysearch";
+    private static final String OUT_JSON = "/json?";
+    private static final String LOG_TAG = "ListRest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_page);
+
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Double lng = Double.parseDouble(longitude);
+        Double lat = Double.parseDouble(latitude);
+        int radius = 1000;
+
+        ArrayList<Place> list = search(lat,lng,radius);
+
+        if (list != null)
+        {
+            resList = (GridView) findViewById(R.id.resListView);
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, list);
+            resList.setAdapter(adapter);
+        }
     }
 }
