fde426c6559c257f696fcc8ed44919c30cb607f9
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-06-29 09:16:10 -0400

    new restaurant page

--- app/src/main/java/com/example/veganbuddyapp/RestaurantsPage.java
+++ app/src/main/java/com/example/veganbuddyapp/RestaurantsPage.java
@@ -2,33 +2,52 @@ package com.example.veganbuddyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ImageSwitcher;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class RestaurantsPage extends AppCompatActivity {
 
     private GridView resList;
     private static final String API_KEY = "AIzaSyAvhvD5YBxPNO2L4bsN745AF8Bi8fpze7w";
 
-    String[] names = {"restaurants name"};
-    int[] images = {R.drawable.ic_launcher_background};
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+
+    private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    private static final String TYPE_DETAILS = "/details";
+    private static final String TYPE_SEARCH = "/nearbysearch";
+    private static final String OUT_JSON = "/json?";
+    private static final String LOG_TAG = "ListRest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_page);
+        Intent intent = getIntent();
+        String longitude = intent.getStringExtra("long");
+        String latitude = intent.getStringExtra("lat");
 
-        gridView = findViewById(R.id.restaurants_name_grid_view);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-        CustomAdapter customAdapter = new CustomAdapter(names,images, context: this);
+        Double lng = Double.parseDouble(longitude);
+        Double lat = Double.parseDouble(latitude);
+        int radius = 10000;
 
         ArrayList<Place> list = search(lat, lng, radius);
 
@@ -40,17 +59,39 @@ public class RestaurantsPage extends AppCompatActivity {
         }
     }
 
-    public class CustomAdapter extends BaseAdapter{
-        private String[] restaurantsNames;
-        private int[] restaurantPhotos;
-        private Context context;
-        private LayoutInflater layoutInflater;
+    public static ArrayList<Place> search(double lat, double lng, int radius) {
+        ArrayList<Place> resultList = null;
 
-        public CustomAdapter(String[] restaurantsNames, int[] restaurantPhotos, Context context) {
-            this.restaurantsNames = restaurantsNames;
-            this.restaurantPhotos = restaurantPhotos;
-            this.context = context;
-            this.layoutInflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE);
+            sb.append(TYPE_SEARCH);
+            sb.append(OUT_JSON);
+            sb.append("location=" + String.valueOf(lat) + "," + String.valueOf(lng));
+            sb.append("&radius=" + String.valueOf(radius));
+            sb.append("&type=restaurant");
+            sb.append("&key=" + API_KEY);
+
+            URL url = new URL(sb.toString());
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error processing Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to PLaces API", e);
+            return resultList;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
         }
 
         try {
@@ -66,39 +107,23 @@ public class RestaurantsPage extends AppCompatActivity {
                 place.name = predsJsonArray.getJSONObject(i).getString("name");
                 resultList.add(place);
             }
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, "Error processing JSON results", e);
-            }
-            return resultList;
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Error processing JSON results", e);
         }
+        return resultList;
+    }
 
-        @Override
-        public Object getItem(int i) {
-            return null;
+    //Value Object for the ArrayList
+    public static class Place {
+        private String reference;
+        private String name;
+
+        public Place(){
+            super();
         }
-
         @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-
-
-            if(view == null){
-                view = LayoutInflater.inflate(R.layout.restaurants_list, viewGroup, attachToRoot: false);
-
-            }
-
-            TextView restaurant_name = view.findViewById(R.id.restaurant_name);
-            ImageView imageView =view.findViewById(R.id.restaurant_image);
-
-            restaurant_name.setText(restaurantsNames[i]);
-            imageView.setImageResource(restaurantPhotos[i]);
-
-
-            return view;
+        public String toString(){
+            return this.name; //This is what returns the name of each restaurant for array list
         }
     }
 }
