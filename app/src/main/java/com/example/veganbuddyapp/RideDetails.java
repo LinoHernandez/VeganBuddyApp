8722c1c3019acb4e1897ea911f78aaa663c945f0
manjitsidhu77 <101609020+manjitsidhu77@users.noreply.github.com>
2022-06-29 08:33:09 -0400

    ride details layout

---
+++ app/src/main/java/com/example/veganbuddyapp/RideDetails.java
@@ -0,0 +1,13 @@
+package com.example.veganbuddyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RideDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_details);
+    }
+}
