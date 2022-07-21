7b0f8ee7a96cffb14c5fd6cb41a185cb6addfbd4
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-07-21 12:16:13 -0400

    ok

--- app/src/main/java/com/example/veganbuddyapp/RestaurantsPage.java
+++ app/src/main/java/com/example/veganbuddyapp/RestaurantsPage.java
@@ -25,6 +25,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
